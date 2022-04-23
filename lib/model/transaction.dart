cd99143d0595eff489edb05642d631cdf65076d2
Pablo Von <pablo-fontao@hotmail.com>
2022-04-23 15:16:43 -0300

    Primeira versão.

---
+++ lib/model/transaction.dart
@@ -0,0 +1,13 @@
+class Transaction {
+  final String id;
+  final String title;
+  final double values;
+  final DateTime date;
+
+  Transaction({
+    required this.id,
+    required this.title,
+    required this.values,
+    required this.date,
+  });
+}
